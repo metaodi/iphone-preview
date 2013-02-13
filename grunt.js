c5914b361d7df2a8133c310382f70ac377748945
Stefan Oderbolz <stefan.oderbolz@liip.ch>
2013-02-13 17:13:19 +0100

    Added build config

---
+++ grunt.js
@@ -0,0 +1,62 @@
+/*global module:false*/
+module.exports = function(grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+        qunit: {
+            files: ['test/index.html']
+        },
+        lint: {
+            src: 'resource/*.js',
+            grunt: 'grunt.js',
+            tests: 'test/*.js'
+        },
+        jshint: {
+            options: {
+                curly: true,
+                eqeqeq: true,
+                immed: true,
+                latedef: true,
+                newcap: true,
+                noarg: true,
+                sub: true,
+                undef: true,
+                unused: false,
+                onevar: true,
+                boss: true,
+                eqnull: true,
+                browser: true,
+                devel: true,
+                trailing: true,
+                white:  false,
+                maxcomplexity: 3
+                /*indent: 4  comment out until white/indent options are separated => https://github.com/jshint/jshint/issues/655 */
+            },
+            tests: {
+                globals: {
+                    asyncTest: true,
+                    deepEqual: true,
+                    equal: true,
+                    expect: true,
+                    module: true,
+                    notDeepEqual: true,
+                    notEqual: true,
+                    notStrictEqual: true,
+                    ok: true,
+                    QUnit: true,
+                    raises: true,
+                    start: true,
+                    stop: true,
+                    strictEqual: true,
+                    test: true
+                }
+            }
+        }
+    });
+
+    // Default task.
+    grunt.registerTask('default', 'lint');
+
+    // Travis CI task.
+    grunt.registerTask('travis', 'lint qunit');
+};
